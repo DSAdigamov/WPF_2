0ff7366c6b993e763989b3f93a933773e255ac6a
DSAdigamov <101574437+DSAdigamov@users.noreply.github.com>
2022-03-28 08:40:54 +0300

    Добавьте файлы проекта.

---
+++ WPF_2/Data.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_2
+{
+    class Data
+    {
+        private string question, answ1, answ2, answ3;
+        private int rightAnsw;
+
+
+        public string Question { get { return question; } set { question = value; } }
+        public string Answ1 { get { return answ1; } set { answ1 = value; } }
+        public string Answ2 { get { return answ2; } set { answ2 = value; } }
+        public string Answ3 { get { return answ3; } set { answ3 = value; } }
+
+        public int RightAnsw { get { return rightAnsw; } set { rightAnsw = value; } }
+
+
+
+        public Data() { }
+
+        public Data(string question, string answ1, string answ2, string answ3, int rightAnsw)
+        {
+            this.question = question;
+            this.answ1 = answ1;
+            this.answ2 = answ2;
+            this.answ3 = answ3;
+            this.rightAnsw = rightAnsw;
+        }
+
+
+    }
+}
---
+++ WPF_2/MainWindow.xaml.cs
@@ -0,0 +1,172 @@
+﻿using Microsoft.VisualBasic.FileIO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace WPF_2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private List<Data> dataList;
+
+        internal List<Data> DataList { get => dataList; set => dataList = value; }
+
+        public int currPos;
+        public int[] choosenAnsw;
+        public int time = 10;
+
+        DispatcherTimer timer = new DispatcherTimer();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            List<Data> list = new List<Data>();
+            using (TextFieldParser parser = new TextFieldParser(@"C:\Users\Даниль\source\repos\WPF_2\WPF_2\WPF_2_Test.csv"))
+            {
+                parser.TextFieldType = FieldType.Delimited;
+                parser.SetDelimiters(";");
+                while (!parser.EndOfData)
+                {
+                    string[] rows = parser.ReadFields();
+                    list.Add(new Data(rows[0], rows[1], rows[2], rows[3], int.Parse(rows[4])));
+                }
+            }
+            DataList = list;
+            currPos = 0;
+            choosenAnsw = new int[list.Count];
+
+            setQuestion(0);
+
+            timer.Start();
+            timer.Tick += new EventHandler(timer_tick);
+            timer.Interval = new TimeSpan(0, 0, 1);
+
+        }
+
+        private void timer_tick(object sender, EventArgs e)
+        {
+            time--;
+            lab1.Content = (time/60).ToString();
+            lab2.Content = (time - time/60*60).ToString();
+            if (time == 0)
+            {
+                getCounter();
+            }
+        }
+
+        private void buttonPrev_Click(object sender, RoutedEventArgs e)
+        {
+            choosenAnsw[currPos] = radioChecker();
+
+            if (currPos != 0)
+            {
+                currPos--;
+                setQuestion(currPos);
+            }
+        }
+
+        private void buttonNext_click(object sender, RoutedEventArgs e)
+        {
+            choosenAnsw[currPos] = radioChecker();
+
+            if (currPos != DataList.Count -1)
+            {
+                currPos++;
+                setQuestion(currPos);
+            }
+        }
+
+        private void button_end_click(object sender, RoutedEventArgs e)
+        {
+            getCounter();
+        }
+
+        private void getCounter()
+        {
+            choosenAnsw[currPos] = radioChecker();
+            int count = 0;
+            for (int i = 0; i < DataList.Count; i++)
+            {
+                if (DataList[i].RightAnsw == choosenAnsw[i])
+                {
+                    count++;
+                }
+            }
+            MessageBox.Show("Правильных ответов: " + count + " из " + DataList.Count);
+        }
+
+        private int radioChecker()
+        {
+            int numb = 0;
+            if (radio1.IsChecked == true)
+            {
+                numb = 1;
+            }
+
+            if (radio2.IsChecked == true)
+            {
+                numb = 2;
+            }
+
+            if (radio3.IsChecked == true)
+            {
+                numb = 3;
+            }
+            return numb;
+        }
+
+        private void setQuestion(int pos)
+        {
+            text1.Text = DataList[pos].Question;
+            radio1.Content = DataList[pos].Answ1;
+            radio2.Content = DataList[pos].Answ2;
+            radio3.Content = DataList[pos].Answ3;
+            if (choosenAnsw[pos] != 0)
+            {
+                switch(choosenAnsw[pos])
+                {
+                    case 1:
+                        radio1.IsChecked = true;
+                        radio2.IsChecked = false;
+                        radio3.IsChecked = false;
+                        break;
+                    case 2:
+                        radio2.IsChecked = true;
+                        radio1.IsChecked= false;
+                        radio3.IsChecked= false;
+                        break;
+                    case 3:
+                        radio3.IsChecked = true;
+                        radio1.IsChecked = false;
+                        radio2.IsChecked= false;
+                        break;
+                }
+            }
+            else
+            {
+                radio1.IsChecked = false;
+                radio2.IsChecked = false;
+                radio3.IsChecked = false;
+            }
+
+        }
+
+
+    }
+}
